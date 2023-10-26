a308b1658dadafe067aa67d7358efa8dadfdae9b
andreaacomelli <comelliandrea21@gmail.com>
2023-10-26 13:20:08 +0200

    Aggiunte

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quiz_app2/main.dart';
+import 'package:Esame_Sicurezza_Aziendale/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
