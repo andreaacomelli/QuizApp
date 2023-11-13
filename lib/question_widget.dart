7f3e92b11ecb04bcbba8565bd89d339225d2da0b
andreaacomelli <comelliandrea21@gmail.com>
2023-11-13 18:39:49 +0100

    risoluzione problemi

--- lib/question_widget.dart
+++ lib/question_widget.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'quiz_response_class.dart';
 
 class QuestionWidget extends StatefulWidget {
   final String questionText;
   final List<String> answerOptions;
+  final List<QuizResponse> answeredQuestionsList;
   final Function(String, int) onAnswerSelected;
-  final Function onNextQuestion;
+  final VoidCallback onNextQuestion;
 
   QuestionWidget({
     required this.questionText,
     required this.answerOptions,
+    required this.answeredQuestionsList,
     required this.onAnswerSelected,
     required this.onNextQuestion,
   });
@@ -18,8 +21,19 @@ class QuestionWidget extends StatefulWidget {
 }
 
 class _QuestionWidgetState extends State<QuestionWidget> {
-  String? _site;
-  int? _selectedIndex;
+  Map<int, int> _selectedAnswers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSelectedAnswers();
+  }
+
+  void _loadSelectedAnswers() {
+    for (var response in widget.answeredQuestionsList) {
+      _selectedAnswers[response.questionIndex] = widget.answerOptions.indexOf(response.selectedAnswer);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,33 +47,37 @@ class _QuestionWidgetState extends State<QuestionWidget> {
             borderRadius: BorderRadius.circular(12),
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Text(
                 widget.questionText,
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 16),
-              Column(
-                children: <Widget>[
-                  for (int index = 0; index < widget.answerOptions.length; index++)
-                    ListTile(
-                      title: Text(
-                        widget.answerOptions[index],
-                        softWrap: true,
-                      ),
-                      leading: Radio<String>(
-                        value: widget.answerOptions[index],
-                        groupValue: _site,
-                        onChanged: (String? value) {
-                          widget.onAnswerSelected(value!, index);
-                          setState(() {
-                            _site = value;
-                            _selectedIndex = index;
-                          });
-                        },
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: widget.answerOptions.length,
+                itemBuilder: (context, index) {
+                  final answer = widget.answerOptions[index];
+                  bool isSelected = _selectedAnswers.containsKey(index);
+
+                  return RadioListTile<int>(
+                    title: Text(
+                      answer,
+                      style: TextStyle(
+                        color: isSelected ? Colors.blue : Colors.black,
                       ),
                     ),
-                ],
+                    value: index,
+                    groupValue: isSelected ? _selectedAnswers[index] : null,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedAnswers[index] = value!;
+                        widget.onAnswerSelected(answer, index);
+                      });
+                    },
+                  );
+                },
               ),
             ],
           ),
@@ -69,7 +87,7 @@ class _QuestionWidgetState extends State<QuestionWidget> {
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 16.0),
             child: Text(
-              'Fai swipe verso destra e sinistra per navigare tra le domande',
+              'Swipe right and left to navigate questions',
               style: TextStyle(fontSize: 12, color: Colors.grey),
               textAlign: TextAlign.center,
             ),
--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 import 'dart:math';
-
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
-
 import 'quiz_response_class.dart';
 import 'question_widget.dart';
 import 'question_class.dart';
@@ -26,15 +24,17 @@ class _QuizScreenState extends State<QuizScreen> {
   int answeredQuestions = 0;
   int selectedOptionIndex = -1;
   int _selectedOptionIndex = -1;
-
+  List<QuizResponse> answeredQuestionsList = [];
   Map<int, QuizResponse> quizResponses = {};
-
   List<Question> shuffledQuestions = [];
 
   void onAnswerSelected(String selectedAnswer, int selectedIndex) {
     final questionData = shuffledQuestions[currentQuestionIndex];
     checkAnswer(selectedAnswer, questionData.correctAnswer);
-    updateQuizResponses(currentQuestionIndex, selectedAnswer);
+
+    if (!answeredQuestionsList.any((element) => element.questionIndex == currentQuestionIndex)) {
+      answeredQuestionsList.add(QuizResponse(questionIndex: currentQuestionIndex, selectedAnswer: selectedAnswer));
+    }
 
     answeredQuestions++;
     setState(() {
@@ -53,10 +53,7 @@ class _QuizScreenState extends State<QuizScreen> {
   }
 
   void updateQuizResponses(int questionIndex, String selectedAnswer) {
-    quizResponses[questionIndex] = QuizResponse(
-      questionIndex: questionIndex,
-      selectedAnswer: selectedAnswer,
-    );
+    quizResponses[questionIndex] = QuizResponse(questionIndex: questionIndex, selectedAnswer: selectedAnswer);
   }
 
   void onNextQuestion() {
@@ -65,26 +62,6 @@ class _QuizScreenState extends State<QuizScreen> {
       if (index! < (maxQuestions - 1)) {
         currentQuestionIndex++;
 
-        print("Page Controller: ${(pageController.page)?.toInt()}");
-        print("Current Question Index: ${currentQuestionIndex}");
-        print("Total Question: ${totalQuestions}");
-        print("Correct Answers: ${correctAnswers}");
-        print("Incorrect Answer: ${incorrectAnswers}");
-        print("Max Questions: ${maxQuestions}");
-        print("Answered Questions: ${answeredQuestions}");
-        print("Selected Option Index: ${selectedOptionIndex}");
-        print("_Selected Option Index: ${_selectedOptionIndex}");
-
-        print("Index: ${index}");
-        print("Max questions: ${maxQuestions - 1}");
-
-        print("Index: ${(index).runtimeType}");
-        print("Max questions: ${(maxQuestions - 1).runtimeType}");
-
-        print("Answered Questions: ${answeredQuestions}");
-
-
-        // Verifica se l'utente ha già risposto a questa domanda
         if (quizResponses.containsKey(currentQuestionIndex)) {
           _selectedOptionIndex = shuffledQuestions[currentQuestionIndex]
               .answerOptions
@@ -99,11 +76,11 @@ class _QuizScreenState extends State<QuizScreen> {
         );
       } else {
         if (answeredQuestions != index) {
-          print("Rispondi a tutte le domande");
-          // L'utente deve rispondere a tutte le domande prima di visualizzare il risultato
-          // Puoi aggiungere qui un messaggio o un comportamento desiderato
+          // print("Answer all the questions");
+          // print(answeredQuestions);
+          // print(index);
+          // print(answeredQuestionsList);
         } else {
-          print("Arrivato!!!");
           Navigator.of(context).pushReplacement(
             MaterialPageRoute(
               builder: (BuildContext context) =>
@@ -113,8 +90,7 @@ class _QuizScreenState extends State<QuizScreen> {
                     totalQuestions: totalQuestions,
                     shuffledQuestions: shuffledQuestions,
                     selectedOptionIndex: _selectedOptionIndex,
-                    answerOptions:
-                    questions[currentQuestionIndex].answerOptions,
+                    answerOptions: questions[currentQuestionIndex].answerOptions,
                     questions: questions,
                     currentQuestion: questions[currentQuestionIndex],
                     quizResponses: quizResponses,
@@ -141,18 +117,15 @@ class _QuizScreenState extends State<QuizScreen> {
   }
 
   Future<void> loadQuestions() async {
-    final String csvData =
-    await rootBundle.loadString('assets/domande.csv');
-    final List<List<dynamic>> csvTable =
-    CsvToListConverter().convert(csvData);
+    final String csvData = await rootBundle.loadString('assets/domande.csv');
+    final List<List<dynamic>> csvTable = CsvToListConverter().convert(csvData);
     final List<Question> questionList = [];
 
     for (var i = 1; i < csvTable.length; i++) {
       final questionData = csvTable[i];
       final questionText = questionData[0] as String;
       final correctAnswer = questionData[1] as String;
-      final answerOptions =
-      List<String>.from(questionData.sublist(2));
+      final answerOptions = List<String>.from(questionData.sublist(2));
 
       answerOptions.shuffle(Random());
 
@@ -186,63 +159,54 @@ class _QuizScreenState extends State<QuizScreen> {
           }
           final questionData = questions[index];
           final questionText = questionData.questionText;
-          final correctAnswer = questionData.correctAnswer;
           final answerOptions = questionData.answerOptions;
+
           return QuestionWidget(
             questionText: questionText,
             answerOptions: answerOptions,
-            onAnswerSelected: (selectedAnswer, selectedIndex) {
-              checkAnswer(selectedAnswer, correctAnswer);
-              setState(() {
-                _selectedOptionIndex = selectedIndex;
-              });
-              onNextQuestion();
-            },
-            onNextQuestion: () {
-              onNextQuestion();
-            },
+            answeredQuestionsList: answeredQuestionsList,
+            onAnswerSelected: onAnswerSelected,
+            onNextQuestion: onNextQuestion,
           );
         },
       ),
-
       floatingActionButton: answeredQuestions >= maxQuestions
           ? Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Hai risposto a tutte le $maxQuestions domande',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 20.0),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => QuizResult(
-                          correctAnswers: correctAnswers,
-                          incorrectAnswers: incorrectAnswers,
-                          totalQuestions: totalQuestions,
-                          shuffledQuestions: shuffledQuestions,
-                          selectedOptionIndex: _selectedOptionIndex,
-                          answerOptions:
-                          questions[currentQuestionIndex].answerOptions,
-                          questions: questions,
-                          currentQuestion: questions[currentQuestionIndex],
-                          quizResponses: quizResponses,
-                        ),
-                      ),
-                    );
-                  },
-                  child: Text('Visualizza il risultato'),
-                ),
-              ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Answered all $maxQuestions questions',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+              textAlign: TextAlign.center,
             ),
-          )
-        : null,
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => QuizResult(
+                      correctAnswers: correctAnswers,
+                      incorrectAnswers: incorrectAnswers,
+                      totalQuestions: totalQuestions,
+                      shuffledQuestions: shuffledQuestions,
+                      selectedOptionIndex: _selectedOptionIndex,
+                      answerOptions: questions[currentQuestionIndex].answerOptions,
+                      questions: questions,
+                      currentQuestion: questions[currentQuestionIndex],
+                      quizResponses: quizResponses,
+                    ),
+                  ),
+                );
+              },
+              child: Text('Show Results'),
+            ),
+          ],
+        ),
+      )
+          : null,
     );
   }
 }
